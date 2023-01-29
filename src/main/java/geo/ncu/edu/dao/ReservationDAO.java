080fec236e68179f4d640bf1296e0deb579a50c4
junting <10355098@st.thsh.tp.edu.tw>
2023-01-30 00:03:13 +0800

    nuc portal

--- src/main/java/geo/ncu/edu/dao/ReservationDAO.java
+++
@@ -1,9 +0,0 @@
-package geo.ncu.edu.dao;
-
-import geo.ncu.edu.enity.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationDAO extends JpaRepository<Reservation,Integer> {
-
-
-}
